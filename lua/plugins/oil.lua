cfa67f77686e8734df069724cee8a657fb5dbc95
Dave <dwiper@idreaminteractive.com>
2026-02-05 15:56:10 -0500

    Add Oil plugin configuration
    Set up Oil plugin with key mapping and options.

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,20 @@
+vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 5,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
