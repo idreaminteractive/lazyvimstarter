d0239e482e1d89dc478f13e4211f00a02d13ed65
Dave <dwiper@idreaminteractive.com>
2026-04-16 12:37:07 -0400

    testing dexter lsp

---
+++ lua/plugins/dexter.lua
@@ -0,0 +1,13 @@
+vim.lsp.config('dexter', {
+  cmd = { 'dexter', 'lsp' },
+  root_markers = { '.dexter.db', '.git', 'mix.exs' },
+  filetypes = { 'elixir', 'eelixir', 'heex' },
+  init_options = {
+    followDelegates = true, -- jump through defdelegate to the target function
+    -- stdlibPath = "",      -- override Elixir stdlib path (auto-detected)
+    -- debug = false,        -- verbose logging to stderr (view with :LspLog)
+  },
+})
+
+vim.lsp.enable 'dexter'
+return {}
