7c47596ffbabe4de40eab8bd597f9a5946e4bcce
Dave <dwiper@idreaminteractive.com>
2026-06-02 13:09:37 -0400

    trging

--- lua/plugins/odin.lua
+++ lua/plugins/odin.lua
@@ -1,1 +1,8 @@
-require("lspconfig").ols.setup({})
+require("lspconfig").ols.setup({
+  init_options = {
+    checker_args = "-strict-style",
+    collections = {
+      { name = "shared", path = vim.fn.expand("$HOME/odin-lib") },
+    },
+  },
+})
